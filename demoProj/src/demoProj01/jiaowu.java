fc530643e205fdf58f58d4df4e1ac4e733687cdf
Administrator <Administrator@MB405-73>
2019-05-14 09:17:39 +0800

    第一次在branch01操作，修改demoProj01的main

--- demoProj/src/demoProj01/jiaowu.java
+++ demoProj/src/demoProj01/jiaowu.java
@@ -1,10 +1,10 @@
-package demoProj;
+package demoProj01;
 
 public class jiaowu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("good!");
+		System.out.println("good branch!");
 
 	}
 
